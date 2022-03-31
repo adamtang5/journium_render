e143b7d9f0a28e0c01761d311a4c199723b86103
Adam Tang <adamtang@gmail.com>
2022-03-31 14:43:47 -0700

    removed GET /hello/world test route

--- backend/phase-5-history.bash
+++ backend/phase-5-history.bash
@@ -1,1 +1,5 @@
 touch utils/validation.js
+
+# implemented validation for log in and sign up request bodies
+
+# removed GET /hello/world test route
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api');
 
+/* Test route during development
+GET /hello/world
 router.get('/hello/world', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
     res.send('Hello World!');
 });
+*/
 
 router.use('/api', apiRouter);
 
