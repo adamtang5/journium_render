7231e5346aa992c609c0cd37aa2b9e211d6392e9
Adam Tang <adamtang@gmail.com>
2022-03-31 12:42:47 -0700

    tested user auth middlewares

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,6 +4,9 @@ router.post('/test', (req, res) => {
     res.json({ requestBody: req.body });
 });
 
+/*
+Testing user auth middlewares
+
 // GET /api/set-token-cookie
 const asyncHandler = require('express-async-handler');
 const { setTokenCookie } = require('../../utils/auth');
@@ -18,5 +21,19 @@ router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
     setTokenCookie(res, user);
     return res.json({ user });
 }));
+
+// GET /api/restore-user
+const { restoreUser } = require('../../utils/auth');
+router.get('/restore-user', restoreUser, (req, res) => {
+    return res.json(req.user);
+});
+
+// GET /api/require-auth
+const { requireAuth } = require('../../utils/auth');
+router.get('/require-auth', requireAuth, (req, res) => {
+    return res.json(req.user);
+});
+
+*/
 
 module.exports = router;
