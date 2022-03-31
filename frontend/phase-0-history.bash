2936d2e31ae526b00b2d66d9ce19b11aa51d0cfc
Adam Tang <adamtang@gmail.com>
2022-03-31 16:09:10 -0700

    set up frontend dependencies
    set up rootReducer, enhancer, configureStore for Redux store

---
+++ frontend/phase-0-history.bash
@@ -0,0 +1,13 @@
+# Choosing Method 1: Set up Redux from scratch
+
+npx create-react-app . --template @appacademy/react-v17 --use-npm
+
+# Dependencies
+npm install js-cookie react-redux react-router-dom@^5 redux redux-thunk
+npm install -D redux-logger
+
+# set up Redux store
+mkdir src/store
+touch src/store/index.js
+
+# fill in src/store/index.js
