564d7c520615b8d4d569575c6c02e173535fc35e
Adam Tang <adamtang@gmail.com>
2022-03-31 10:01:50 -0700

    Phase 2 Error Handling - complete

--- backend/phase-2-history.bash
+++ backend/phase-2-history.bash
@@ -3,3 +3,10 @@
 # 404 handler
 
 # sequelize error handler
+
+# error formatter handler
+
+# test 404 error handler
+# tested working routes:
+## GET /hello/world
+## POST /api/test with XSRF-TOKEN and json body
