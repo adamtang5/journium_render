0e09a09e821ef67041b57fa9275bb869523a2778
Adam Tang <adamtang@gmail.com>
2022-03-31 09:55:45 -0700

    implemented last error formatter handler in app.js

--- backend/app.js
+++ backend/app.js
@@ -50,6 +50,15 @@ app.use(
 
 app.use(routes);    // Connect all the routes
 
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+});
+
 // Process sequelize errors
 app.use((err, _req, _res, next) => {
     // check if error is a Sequelize error:
@@ -60,13 +69,16 @@ app.use((err, _req, _res, next) => {
     next(err);
 });
 
-// Catch unhandled requests and forward to error handler.
-app.use((_req, _res, next) => {
-    const err = new Error("The requested resource couldn't be found.");
-    err.title = "Resource Not Found";
-    err.errors = ["The requested resource couldn't be found."];
-    err.status = 404;
-    next(err);
+// Error formatter
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack,
+    });
 });
 
 module.exports = app;
