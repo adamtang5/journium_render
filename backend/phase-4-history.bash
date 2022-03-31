f18a40dabab43e8fe7f2a78910f50eb916f4db0b
Adam Tang <adamtang@gmail.com>
2022-03-31 13:01:04 -0700

    implemented and tested login route

--- backend/phase-4-history.bash
+++ backend/phase-4-history.bash
@@ -1,1 +1,7 @@
 # Implement authentication API routes
+
+# User Auth API routes
+# Login: POST /api/session
+# Logout: DELETE /api/session
+# Signup: POST /api/users
+# Get session user: GET /api/session
