0f50468be5f333b5eccac71af8d23b36daad1789
Adam Tang <adamtang@gmail.com>
2022-04-01 16:03:53 -0700

    implemented ProfileButton component

--- frontend/phase-3-history.bash
+++ frontend/phase-3-history.bash
@@ -4,3 +4,5 @@ mkdir src/components/Navigation
 touch src/components/Navigation/index.js
 
 # Added navigation links to home, logout, login, signup
+
+# Added ProfileButton component and tested
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,32 +1,38 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import ProfileButton from '../ProfileButton';
 
 const Navigation = () => {
     const sessionUser = useSelector(state => state.session.user);
 
     return (
-        <ul>
-            <li key="home">
-                <NavLink activeClassName="active" to="/">
-                    Home
-                </NavLink>
-            </li>
-            {sessionUser ? (
-                <li key="logout">
-                    <NavLink to="/logout">Log Out</NavLink>
+        <>
+            <ul>
+                <li key="home">
+                    <NavLink activeClassName="active" to="/">
+                        Home
+                    </NavLink>
                 </li>
-            ) : (
-                <>
-                    <li key="login">
-                        <NavLink to="/login">Log In</NavLink>
+                {sessionUser ? (
+                    <li key="logout">
+                        <NavLink to="/logout">Log Out</NavLink>
                     </li>
-                    <li key="signup">
-                        <NavLink to="/signup">Sign Up</NavLink>
-                    </li>
-                </>
+                ) : (
+                    <>
+                        <li key="login">
+                            <NavLink to="/login">Log In</NavLink>
+                        </li>
+                        <li key="signup">
+                            <NavLink to="/signup">Sign Up</NavLink>
+                        </li>
+                    </>
+                )}
+            </ul>
+            {sessionUser && (
+                <ProfileButton />
             )}
-        </ul>
+        </>
     )
 };
 
