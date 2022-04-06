36c47fee3fb4b97a9a991308582795935ccee09c
Adam Tang <adamtang@gmail.com>
2022-04-06 11:34:01 -0700

    styled Stories page, StoryCard component

--- frontend/src/components/StoryCard/index.js
+++ frontend/src/components/StoryCard/index.js
@@ -8,27 +8,42 @@ const StoryCard = ({ storyId }) => {
 
     return (
         <div className="story-card">
+            <div className="author flex-row">
+                <Link className="avatar" to={`/users/${story.userId}`}>
+                    <img src={story.User.avatarUrl} alt={story.User.displayName} />
+                </Link>
+                <Link className="display-name" to={`/users/${story.User.id}`}>
+                    <p>{story.User.displayName}</p>
+                </Link>
+                <Link className={`role role-${(storyId % 5 === 0) ? 5 : storyId % 5}`} to={`/users/${story.User.id}`}>
+                    <span>{story.User.Role.name}</span>
+                </Link>
+            </div>
             <div className="story">
-                <div className="story-author">
-                    <Link className="avatar" to={`/users/${story.userId}`}>
-                        <img src={story.User.avatarUrl} alt={story.User.displayName} />
-                    </Link>
-                    <div className="author_org">
-                        <Link className="author_name" to={`/users/${story.User.id}`}>
-                            <h4>{story.User.displayName}</h4>
-                            <span className="role">{story.User.Role.name}</span>
-                        </Link>
-                    </div>
-                </div>
                 <div className="title">
                     <Link to={`/stories/${story.id}`}>
                         <h2>{story.title}</h2>
                     </Link>
                 </div>
-                <div className="story-metrics">
-                    <div className="date">{story.createAt}</div>
-                    <span>·</span>
+                <div className="blurb">
+                    <Link to={`/stories/${story.id}`}>
+                        <p>{story.content}</p>
+                    </Link>
                 </div>
+            </div>
+            <div className="thumbnail">
+                <Link to={`/stories/${story.id}`}>
+                    <img src={story.imageUrl} alt={story.title} />
+                </Link>
+            </div>
+            <div className="info flex-row">
+                {/* <Link to={`/tags/${tag.id}`}>
+                    <div className="tag">{tag.name}</div>
+                </Link> */}
+                <Link to={`/stories/${story.id}`}>
+                    <div className="date">{new Date(story.createdAt).toString().slice(4, 10)}</div>
+                    <span>·</span>
+                </Link>
             </div>
         </div>
     )
