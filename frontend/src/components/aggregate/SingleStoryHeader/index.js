840050ebd0a74e1c21c3ad709cc6358f1e1732d0
Adam Tang <adamtang@gmail.com>
2022-04-07 15:31:30 -0700

    added edit button for owner on single story page

--- frontend/src/components/aggregate/SingleStoryHeader/index.js
+++ frontend/src/components/aggregate/SingleStoryHeader/index.js
@@ -1,19 +1,32 @@
+import { useSelector } from "react-redux";
+import { Link } from 'react-router-dom';
 import Avatar from "../../atomic/Avatar";
 import DisplayName from '../../atomic/DisplayName';
 import Role from '../../atomic/Role';
 import StoryDate from '../../atomic/StoryDate';
 
 const SingleStoryHeader = ({ story }) => {
+    const sessionUser = useSelector(state => state.session.user);
+
     return (
-        <div className="story-info flex-row">
-            <Avatar user={story.User} />
-            <div className="story-info-text flex-column">
-                <div className="author-info flex-row">
-                    <DisplayName user={story.User} />
-                    <Role user={story.User} />
+        <div className="story-header flex-row">
+            <div className="story-info flex-row">
+                <Avatar user={story.User} />
+                <div className="story-info-text flex-column">
+                    <div className="author-info flex-row">
+                        <DisplayName user={story.User} />
+                        <Role user={story.User} />
+                    </div>
+                    <StoryDate story={story} />
                 </div>
-                <StoryDate story={story} />
             </div>
+            {(sessionUser.id === story.userId) ? (
+                <div className="story-owner-action">
+                    <Link to={`/stories/${story.id}/edit`}>
+                        <button className="edit">Edit</button>
+                    </Link>
+                </div>
+            ) : null}
         </div>
     )
 };
