6ae0c54ced6a9b389dcfd610e16c599dc571606e
Adam Tang <adamtang@gmail.com>
2022-04-08 14:14:21 -0700

    fixed POST /api/comments issue, completed and tested POST, PUT and DELETE API routes

--- backend/routes/api/comments.js
+++ backend/routes/api/comments.js
@@ -7,14 +7,8 @@ const db = require('../../db/models');
 const router = express.Router();
 
 // POST /api/comments
-router.post('/', requireAuth, asyncHandler(async (res, req) => {
-    console.log(req.body);
-    const { userId, storyId, content } = req.body;
-    const comment = await db.Comment.create({
-        userId,
-        storyId,
-        content,
-    });
+router.post('/', requireAuth, asyncHandler(async (req, res) => {
+    const comment = await db.Comment.create(req.body);
 
     const returnComment = await db.Comment.findByPk(comment.id, {
         include: {
@@ -24,6 +18,56 @@ router.post('/', requireAuth, asyncHandler(async (res, req) => {
     });
     res.json(returnComment);
 
+}));
+
+// Comment Not Found Error
+const commentNotFoundError = id => {
+    const err = Error(`Comment with id of ${id} could not be found.`);
+    err.title = "Comment not found.";
+    err.status = 404;
+    return err;
+};
+
+// PUT /api/comments/:id
+router.put('/:id(\\d+)', requireAuth, asyncHandler(async (req, res, next) => {
+    const commentId = parseInt(req.params.id, 10);
+    const comment = await db.Comment.findByPk(commentId);
+
+    if (comment) {
+        if (req.user.id === comment.userId) {
+            await comment.update({
+                content: req.body.content,
+            });
+            const returnComment = await db.Comment.findByPk(commentId, {
+                include: {
+                    model: db.User,
+                    include: db.Role,
+                },
+            });
+            res.json(returnComment);
+        } else {
+            next(unauthorizedUserError());
+        }
+    } else {
+        next(commentNotFoundError(commentId));
+    }
+}));
+
+// DELETE /api/comments/:id
+router.delete('/:id(\\d+)', requireAuth, asyncHandler(async (req, res, next) => {
+    const id = parseInt(req.params.id, 10);
+    const comment = await db.Comment.findByPk(id);
+
+    if (comment) {
+        if (req.user.id === comment.userId) {
+            await comment.destroy();
+            res.status(204).end();
+        } else {
+            next(unauthorizedUserError());
+        }
+    } else {
+        next(commentNotFoundError(id));
+    }
 }));
 
 module.exports = router;
