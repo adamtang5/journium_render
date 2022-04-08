431cc2ab4b2eed20a5c86efdc0eb98db00051f35
Adam Tang <adamtang@gmail.com>
2022-04-08 01:37:13 -0700

    implemented 404 page

--- frontend/src/components/aggregate/SingleStory/index.js
+++ frontend/src/components/aggregate/SingleStory/index.js
@@ -2,20 +2,27 @@ import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import SingleStoryHeader from "../SingleStoryHeader";
 import SingleStoryMain from "../SingleStoryMain";
-import SingleStoryFooter from "..//SingleStoryFooter";
+import SingleStoryFooter from "../SingleStoryFooter";
+import PageNotFound from '../../atomic/PageNotFound';
 import './SingleStory.css';
 
 const SingleStory = () => {
     const { id } = useParams();
     const story = useSelector(state => state.story.stories[+id]);
 
-    return (
-        <div className="single-story flex-column">
-            <SingleStoryHeader story={story} />
-            <SingleStoryMain story={story} />
-            <SingleStoryFooter story={story} />
-        </div>
-    )
+    if (story) {
+        return (
+            <div className="single-story flex-column">
+                <SingleStoryHeader story={story} />
+                <SingleStoryMain story={story} />
+                <SingleStoryFooter story={story} />
+            </div>
+        )
+    } else {
+        return (
+            <PageNotFound />
+        )
+    }
 };
 
 export default SingleStory;
