b97fb83d1d953edd34d501da92cc1bcc196eaeb2
Adam Tang <adamtang@gmail.com>
2022-04-11 08:27:20 -0700

    removed dead links to user profile page

--- frontend/src/components/aggregate/StoriesListCardHeader/index.js
+++ frontend/src/components/aggregate/StoriesListCardHeader/index.js
@@ -6,15 +6,15 @@ import Role from "../../atomic/Role";
 const StoriesListCardHeader = ({ story }) => {
     return (
         <div className="author flex-row">
-            <Link to={`/users/${story.userId}`}>
-                <Avatar user={story.User} />
-            </Link>
-            <Link to={`/users/${story.User.id}`}>
-                <DisplayName user={story.User} />
-            </Link>
-            <Link to={`/users/${story.User.id}`}>
-                <Role user={story.User} />
-            </Link>
+            {/* <Link to={`/users/${story.userId}`}> */}
+            <Avatar user={story.User} />
+            {/* </Link> */}
+            {/* <Link to={`/users/${story.User.id}`}> */}
+            <DisplayName user={story.User} />
+            {/* </Link> */}
+            {/* <Link to={`/users/${story.User.id}`}> */}
+            <Role user={story.User} />
+            {/* </Link> */}
         </div>
     )
 };
