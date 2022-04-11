c1c37d1f8ea5a980cd6f5f31313dbabe0328e7f1
Adam Tang <adamtang@gmail.com>
2022-04-11 07:39:40 -0700

    added no image placeholder for stories page

--- frontend/src/components/atomic/Thumbnail/index.js
+++ frontend/src/components/atomic/Thumbnail/index.js
@@ -1,7 +1,11 @@
 const Thumbnail = ({ story }) => {
     return (
         <div className="thumbnail">
-            <img src={story.imageUrl} alt={story.title} />
+            {(story.imageUrl) ? (
+                <img src={story.imageUrl} alt={story.title} />
+            ) : (
+                <img src="/images/no-image.jpg" alt={story.title} />
+            )}
         </div>
     )
 };
