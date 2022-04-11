b85a0a501f7d8d0466c0dc2ffd02aab558df9ce5
Adam Tang <adamtang@gmail.com>
2022-04-11 02:57:23 -0700

    user nav is now sticky

--- frontend/src/components/atomic/RenderImage/index.js
+++ frontend/src/components/atomic/RenderImage/index.js
@@ -2,7 +2,7 @@ const RenderImage = ({ visible, imageUrl, onClick }) => {
     if (!visible) return null;
     return (
         <div className="render-image flex-row" onClick={onClick} >
-            <img src={imageUrl} alt="rendered" />
+            <img src={imageUrl} alt="rendered" title="Click image to change URL" />
         </div>
     )
 };
